5b9ae69708aa314d0e5c2d65f9b66b7c1f8ba1c7
bassarcy <bassarcy@gmail.com>
2023-09-25 13:23:00 -0500

    Added Meridian text/function and fixed some formatting

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const date = new Date();
 
-    function adjustHours(date) {
-        if(date.getHours() > 12) {
-            return date.getHours() - 12;
-        } else if(date.getHours() == 0) {
-            return date.getHours() + 12;
-        } else {
-            return date.getHours();
-        };
+function adjustHours(date) {
+    if(date.getHours() > 12) {
+        return date.getHours() - 12;
+    } else if(date.getHours() == 0) {
+        return date.getHours() + 12;
+    } else {
+        return date.getHours();
     };
+};
 
-    function adjustMinutes(date) {
-        if(date.getMinutes() < 10) {
-            return `0${date.getMinutes()}`;
-        } else {
-            return date.getMinutes();
-        };
+function adjustMinutes(date) {
+    if(date.getMinutes() < 10) {
+        return `0${date.getMinutes()}`;
+    } else {
+        return date.getMinutes();
     };
+};
 
-    function tellTime() {
-        document.getElementById("time").textContent = `${adjustHours(date)}:${adjustMinutes(date)}`
-    }
+function setMeridian(date) {
+    if(date.getHours() < 12 || date.getHours() == 24) {
+        return "am";
+    } else {
+        return "pm";
+    };
+};
 
-    console.log(adjustHours(date));
-    console.log(adjustMinutes(date));
+function tellTime() {
+    document.getElementById("time").textContent = `${adjustHours(date)}:${adjustMinutes(date)} ${setMeridian(date)}`
+};
 
-    tellTime()
+console.log(adjustHours(date));
+console.log(adjustMinutes(date));
+
+tellTime();
